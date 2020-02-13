1649a1c9a2013c075bf601dc52e7086ed5078ca7
Pusheen <crosfake@gmail.com>
2020-02-13 15:24:15 +0200

    Minor Changes

--- HaloArmory.API/Dto/ItemsForDisplayDto.cs
+++ HaloArmory.API/Dto/ItemsForDisplayDto.cs
@@ -2,6 +2,8 @@ namespace HaloArmory.API.Dto
 {
     public class ItemsForDisplayDto
     {
+        public string Type { get; set; }
+        public string Durability { get; set; }
          public string Name { get; set; }
          public string Description { get; set; }
          public int Price { get; set; }
