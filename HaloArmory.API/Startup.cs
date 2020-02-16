3123e25433ec7cffec8ecd2df327a4c535da3a57
Pusheen <crosfake@gmail.com>
2020-02-16 19:01:31 +0200

    Remove redundant code

--- HaloArmory.API/Startup.cs
+++ HaloArmory.API/Startup.cs
@@ -35,13 +35,7 @@ namespace HaloArmory.API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
-            }
 
-            // app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
             app.UseMvc();
         }
